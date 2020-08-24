324b6abbdaeda21e8f030a8072c163986cc117e3
azoumi <hafid.azoumi@accenture.com>
2020-08-24 13:55:48 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: azoumi <hafid.azoumi@accenture.com>

--- platforms/corda-ent/charts/nmap/files/set-network-parameters.sh
+++ platforms/corda-ent/charts/nmap/files/set-network-parameters.sh
@@ -7,9 +7,9 @@ echo "Waiting for notary-nodeinfo/network-parameters-initial.conf ..."
 echo "========================================"
 ls {{ .Values.configPath }}
 echo {{ .Values.configPath }}
-cat /opt/corda/etc/network-parameters-initial-set-succesfully
+cat {{ .Values.configPath }}/network-parameters-initial-set-succesfully
 echo "========================================"
-if [ ! -f /opt/corda/etc/network-parameters-initial-set-succesfully ]
+if [ ! -f {{ .Values.configPath }}/network-parameters-initial-set-succesfully ]
 then
     until [ -f notary-nodeinfo/network-parameters-initial.conf ]
     do
