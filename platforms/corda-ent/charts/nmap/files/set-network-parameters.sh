7de78e854cbf40b7f01f0799fb93f182ccb5489e
azoumi <hafid.azoumi@accenture.com>
2020-08-25 09:52:46 +0000

    [ci skip] Deleting gitops release files
    Signed-off-by: azoumi <hafid.azoumi@accenture.com>

--- platforms/corda-ent/charts/nmap/files/set-network-parameters.sh
+++ platforms/corda-ent/charts/nmap/files/set-network-parameters.sh
@@ -4,11 +4,7 @@ set -x
 {{ end }}
 
 echo "Waiting for notary-nodeinfo/network-parameters-initial.conf ..."
-echo "========================================"
-ls {{ .Values.configPath }}
-echo {{ .Values.configPath }}
-cat {{ .Values.configPath }}/network-parameters-initial-set-succesfully
-echo "========================================"
+
 if [ ! -f {{ .Values.configPath }}/network-parameters-initial-set-succesfully ]
 then
     until [ -f notary-nodeinfo/network-parameters-initial.conf ]
@@ -21,7 +17,6 @@ echo "Waiting for notary-nodeinfo/network-parameters-initial.conf ... done."
 ls -al notary-nodeinfo/network-parameters-initial.conf
 cp notary-nodeinfo/network-parameters-initial.conf {{ .Values.configPath }}/
 cat {{ .Values.configPath }}/network-parameters-initial.conf
-
 
 echo "Setting initial network parameters ..."
 java -jar {{ .Values.jarPath }}/networkmap.jar \
@@ -42,20 +37,13 @@ then
     echo "Going to sleep for requested ${HOW_LONG} seconds to let you login and investigate."
     echo
 else
-    HOW_LONG=0 
-    # {{ .Values.sleepTime }} needs to be filled in
+    HOW_LONG={{ .Values.sleepTime }}
     echo
     echo "Network Map: initial network parameters have been set."
     echo "No errors."
     echo "Sleeping for requested ${HOW_LONG} seconds before disappearing."
     echo
-    # touch {{ .Values.configPath }}/network-parameters-initial-set-succesfully
-    echo "Writing something to file"
-    echo 'content in here' > {{ .Values.configPath }}/network-parameters-initial-set-succesfully
-    ls {{ .Values.configPath }}
-    cat {{ .Values.configPath }}/network-parameters-initial-set-succesfully
-    realpath {{ .Values.configPath }}/network-parameters-initial-set-succesfully
-    echo "Done writing something to file"
+    touch {{ .Values.configPath }}/network-parameters-initial-set-succesfully
     echo "# This is a file with _example_ content needed for updating network parameters" > {{ .Values.configPath }}/network-parameters-update-example.conf
     cat {{ .Values.configPath }}/network-parameters-initial.conf >> {{ .Values.configPath }}/network-parameters-update-example.conf
 cat << EOF >> {{ .Values.configPath }}/network-parameters-update-example.conf
